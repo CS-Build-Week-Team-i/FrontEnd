a55e879cfd822f3a87fb2c287bea820e675687be
Navid Karami <navid.gsz@gmail.com>
2019-09-25 08:52:22 -0700

    setup private route and signup page

--- mud/src/App.js
+++ mud/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.scss';
+import { Switch, Route } from 'react-router-dom';
 import Login from './Component/Login/Login';
+import Signup from './Component/Signup/Signup';
+import Map from './Component/Map/Map';
+import PrivateRoute from './helper/PrivateRoute';
 
 function App() {
   return (
     <div className='App'>
-      <Login />
+      <Switch>
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={Signup} />
+        <PrivateRoute path='/' component={Map} />
+      </Switch>
     </div>
   );
 }
---
+++ mud/src/Component/Map/Map.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Map extends React.Component {
+  render() {
+    return <h1>This is the map</h1>;
+  }
+}
+
+export default Map;
