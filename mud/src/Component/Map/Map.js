636abfde67338049a53434db00cf2f1debfc18fc
HitzeD <d-hitze@comcast.net>
2019-09-26 16:33:36 -0400

    base format

--- mud/src/Component/Map/Map.js
+++ mud/src/Component/Map/Map.js
@@ -1,8 +1,52 @@
 import React from 'react';
 
+import './Map.scss';
+
 class Map extends React.Component {
   render() {
-    return <h1>This is the map</h1>;
+    return (
+      <div className="main-map">
+        <div className="map-header">
+        {/* Top portion in this div */}
+          <h1>This is the map</h1>
+        </div>
+        {/* split these bottom divs ~75%/25% for each side respectively */}
+
+        <div className="map-bottom">
+
+          <div className="map-left">
+            {/* Left side width ~ 75% */}
+            {/* Game view will also go inside here */}
+          </div>
+
+          <div className="map-right">
+
+            <div className="right-header">
+              <h1>Welcome Player!</h1> {/* Change player to username */}
+            </div>
+
+            <hr className="line"/>
+
+            <div className="bottom-info">
+
+              <div className="room-info">
+                <h3>Room: (room stuff here)</h3>
+                <p>Room description will go here. It could be long, so i am just going to type a little bit to make a decent lengthparagraph that would be comparable! Good!</p>
+              </div>
+
+              <div className="player-info">
+                <h3>Name: User</h3>
+                <h3>Items: Stick, Paper, String, Kleenex</h3>
+                <h3>Health: 100</h3>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    )
   }
 }
 
