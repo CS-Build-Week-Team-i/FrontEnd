cba5edadecf2953633671a70cfe5d8acee698c6a
HitzeD <d-hitze@comcast.net>
2019-09-26 19:27:20 -0400

    action built out

--- mud/src/actions/player.js
+++ mud/src/actions/player.js
@@ -7,6 +7,12 @@ export const PLAYER_MOVE_FAILURE = 'PLAYER_MOVE_FAILURE';
 export const playerMove = dir => dispatch => {
     dispatch({ type: PLAYER_MOVE_START });
     return axios.post('https://lambda-mud-test.herokuapp.com/api/adv/move', dir)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+    .then(res => {
+        dispatch({ type: PLAYER_MOVE_SUCCESS, payload: res.data })
+        console.log(res)
+    })
+    .catch(err => {
+        console.log(err)
+        dispatch({ type: PLAYER_MOVE_FAILURE, payload: err })
+    });
 }
