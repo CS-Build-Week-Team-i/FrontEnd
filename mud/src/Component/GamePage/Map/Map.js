79a107d29a2155f80290fcf77e304e54f22ed37d
Krisli Dimo <krislidimo@gmail.com>
2019-09-27 09:42:19 -0500

    moved Map and room into GamePage

--- mud/src/Component/GamePage/Map/Map.js
+++ mud/src/Component/GamePage/Map/Map.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import { grid } from '../../data';
 import { Room, EmptyRoom } from './Room/Room';
 import './Map.scss';
 
-const test = {
-  north: 't',
-  south: 's'
-};
 class Map extends React.Component {
   state = {
-    grid: grid
-  };
+  }
 
   render() {
     return (
       <div className='map'>
-        {this.state.grid.map(row => {
+        {/*{this.state.grid.map(row => {
           return row.map(room => {
             return <Room {...room} />;
           });
-        })}
+        })}*/}
       </div>
-    );
+    )
   }
 }
 
